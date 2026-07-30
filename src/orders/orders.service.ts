b384965cb46bf42ee47d9e4743aca3891a62a849
Otavio Costa <dev.otaviocostao@gmail.com>
2026-07-29 23:18:21 -0300

    fix: corrigindo problemas na aplicação de desconto

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -155,10 +155,8 @@ export class OrdersService {
         calculatedTotal += itemDto.quantity * itemDto.unitPrice;
       }
 
-      const discount = updateOrderDto.discount || 0;
-      const finalValue = calculatedTotal - discount;
       order.items = orderItems;
-      order.totalValue = finalValue;
+      order.totalValue = calculatedTotal;
     }
 
     if (updateOrderDto.payments) {
